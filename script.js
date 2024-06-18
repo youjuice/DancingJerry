12e92b1027619cfc17753c1db15ac970b66194ed
yooju00 <yooju00@naver.com>
2024-06-18 20:55:37 +0900

    Add Youtube Embeded func.

--- script.js
+++ script.js
@@ -16,13 +16,16 @@ window.addEventListener('click', function(event) {
     }
 });
 
-function displaySongRecommendations() {
+function displaySongRecommendations(event) {
+    event.preventDefault();
+
+    const genre = document.getElementById('genre').value;
+    const artist = document.getElementById('artist').value;
+    const title = document.getElementById('title').value;
+
     const songList = [
-        '노래 1',
-        '노래 2',
-        '노래 3',
-        '노래 4',
-        '노래 5'
+        `[${genre}] ♫ "${title}" by ${artist}`,
+        `Thank you ʕتʔ`
     ];
 
     const songListContainer = document.getElementById('songList');
@@ -35,14 +38,48 @@ function displaySongRecommendations() {
     });
 }
 
+function showYouTubeVideo() {
+    var youtubeLink = document.getElementById("youtubeLink").value;
+    var videoContainer = document.getElementById("videoContainer");
+
+    var videoId;
+    if (youtubeLink.includes('youtu.be')) {
+        var startIndex = youtubeLink.lastIndexOf('/') + 1;
+        videoId = youtubeLink.substring(startIndex);
+    } else if (youtubeLink.includes('v=')) {
+        var startIndex = youtubeLink.indexOf('v=') + 2;
+        var endIndex = youtubeLink.indexOf('&', startIndex);
+        videoId = endIndex === -1 ? youtubeLink.substring(startIndex) : youtubeLink.substring(startIndex, endIndex);
+    }
+
+    if (videoId) {
+        var iframe = document.createElement("iframe");
+        iframe.width = "560";
+        iframe.height = "315";
+        iframe.src = "https://www.youtube.com/embed/" + videoId;
+        iframe.frameBorder = "0";
+        iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+        iframe.allowFullscreen = true;
+        iframe.style.width = "100%";
+        iframe.style.height = "315px";
+
+        videoContainer.innerHTML = "";
+        videoContainer.appendChild(iframe);
+    } else {
+        videoContainer.innerHTML = "Invalid YouTube URL";
+    }
+    return false;
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     const openModalBtn = document.getElementById('openModalBtn');
     const closeBtn = document.querySelector('.close');
+    const recommendationForm = document.getElementById('recommendationForm');
 
     openModalBtn.addEventListener('click', openModal);
     closeBtn.addEventListener('click', closeModal);
+    recommendationForm.addEventListener('submit', displaySongRecommendations);
 
-    // 전역 스코프에 함수 노출
     window.openModal = openModal;
     window.closeModal = closeModal;
 });
