fd3eb77f52abc1cafb9894c0577f93d126056279
yooju00 <yooju00@naver.com>
2024-06-19 22:00:13 +0900

    card.html에 Three.js 애니메이션 추가

--- card_Three.js
+++ card_Three.js
@@ -1,35 +1,51 @@
 import * as THREE from 'three';
 import './card.css';
+import {danceJerry, loadDancingJerryModel} from "./counter.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / (window.innerHeight / 3), 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+const light = new THREE.DirectionalLight(0xffffff, 1);
 
-renderer.setSize(window.innerWidth, window.innerHeight / 3);
-document.getElementById('animationSection').appendChild(renderer.domElement);
-
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
+light.position.set(0, 10, 10);
 camera.position.z = 5;
+scene.add(light);
+
+let mixer;
+let dancingJerryModel;
+const clock = new THREE.Clock();
+
+function setRendererSize() {
+    const animationSection = document.getElementById('animationSection');
+    const width = animationSection.clientWidth;
+    const height = animationSection.clientHeight;
+    renderer.setSize(width, height);
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+}
+
+document.getElementById('animationSection').appendChild(renderer.domElement);
+window.addEventListener('resize', () => {
+    setRendererSize();
+});
+
+loadDancingJerryModel((model) => {
+    dancingJerryModel = model;
+    scene.add(dancingJerryModel);
+
+    mixer = new THREE.AnimationMixer(dancingJerryModel);
+    const action = mixer.clipAction(model.animations[0]);
+    action.play();
+});
 
 function animate() {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    const delta = clock.getDelta();
+    danceJerry(delta * 15);
 
     renderer.render(scene, camera);
 }
 
 animate();
-
-window.addEventListener('resize', () => {
-    const width = window.innerWidth;
-    const height = window.innerHeight / 3;
-    renderer.setSize(width, height);
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-});
+setRendererSize();
